e98262d876a863a3d52ae886869bb564426ce343
gregahpradana <91198377+gregahpradana@users.noreply.github.com>
2021-09-22 19:41:17 +0700

    Update spotify.py

--- spotify.py
+++ spotify.py
@@ -18,7 +18,7 @@ genius = lyricsgenius.Genius('biZZReO7F98mji5oz3cE0FiIG73Hh07qoXSIzYSGNN3GBsnY-e
 if 'BOT_TOKEN' in os.environ:
     token = os.environ.get('BOT_TOKEN')
 else:
-    token = 'token bot'
+    token = '2008382589:AAH42C7qzpHdcnbD2BZaFK_iGqGuonwmudE'
 
 bot = telepot.Bot(token)
 
